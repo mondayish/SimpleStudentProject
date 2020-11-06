d2930f5b4f32386d3a5af38dbb83ca3626a71ae1
mondayish <misha140501@mail.ru>
2020-11-06 22:28:48 +0300

    Add table from angular material

--- frontend/src/app/components/app.component.ts
+++ frontend/src/app/components/app.component.ts
@@ -9,13 +9,14 @@ import {Student} from "../models/student";
 })
 
 export class AppComponent {
-    @ViewChild('readOnlyTemplate', {static: false}) readOnlyTemplate: TemplateRef<any>;
-    @ViewChild('editTemplate', {static: false}) editTemplate: TemplateRef<any>;
+    // @ViewChild('readOnlyTemplate', {static: false}) readOnlyTemplate: TemplateRef<any>;
+    // @ViewChild('editTemplate', {static: false}) editTemplate: TemplateRef<any>;
 
-    editedStudent: Student;
-    students: Array<Student>;
-    isNewRecord: boolean;
-    statusMessage: string;
+    // editedStudent: Student;
+    students: Student[];
+    displayedColumns: string[] = ["id", "firstName", "lastName", "age"];
+    // isNewRecord: boolean;
+    // statusMessage: string;
 
     constructor(private studentService: StudentService) {
         this.students = new Array<Student>();
@@ -31,53 +32,53 @@ export class AppComponent {
         });
     }
 
-    addStudent(): void {
-        this.editedStudent = new Student(0, "", "", 0);
-        this.students.push(this.editedStudent);
-        this.isNewRecord = true;
-    }
-
-    editStudent(student: Student): void {
-        this.editedStudent = new Student(student.id, student.firstName, student.lastName, student.age);
-    }
-
-    loadTemplate(student: Student): TemplateRef<any> {
-        if (this.editedStudent && this.editedStudent.id === student.id) {
-            return this.editTemplate;
-        } else {
-            return this.readOnlyTemplate;
-        }
-    }
-
-    saveStudent(): void {
-        if (this.isNewRecord) {
-            this.studentService.createStudent(this.editedStudent).subscribe(data => {
-                this.statusMessage = 'Student was successfully added';
-                this.loadStudents();
-            });
-            this.isNewRecord = false;
-            this.editedStudent = null;
-        } else {
-            this.studentService.updateStudent(this.editedStudent).subscribe(data => {
-                this.statusMessage = 'Student was successfully updated';
-                this.loadStudents();
-            });
-            this.editedStudent = null;
-        }
-    }
-
-    cancel(): void {
-        if (this.isNewRecord) {
-            this.students.pop();
-            this.isNewRecord = false;
-        }
-        this.editedStudent = null;
-    }
-
-    deleteStudent(student: Student): void {
-        this.studentService.deleteStudent(student.id).subscribe(data => {
-            this.statusMessage = 'Student was successfully deleted';
-            this.loadStudents();
-        });
-    }
+    // addStudent(): void {
+    //     this.editedStudent = new Student(0, "", "", 0);
+    //     this.students.push(this.editedStudent);
+    //     this.isNewRecord = true;
+    // }
+    //
+    // editStudent(student: Student): void {
+    //     this.editedStudent = new Student(student.id, student.firstName, student.lastName, student.age);
+    // }
+    //
+    // loadTemplate(student: Student): TemplateRef<any> {
+    //     if (this.editedStudent && this.editedStudent.id === student.id) {
+    //         return this.editTemplate;
+    //     } else {
+    //         return this.readOnlyTemplate;
+    //     }
+    // }
+    //
+    // saveStudent(): void {
+    //     if (this.isNewRecord) {
+    //         this.studentService.createStudent(this.editedStudent).subscribe(data => {
+    //             this.statusMessage = 'Student was successfully added';
+    //             this.loadStudents();
+    //         });
+    //         this.isNewRecord = false;
+    //         this.editedStudent = null;
+    //     } else {
+    //         this.studentService.updateStudent(this.editedStudent).subscribe(data => {
+    //             this.statusMessage = 'Student was successfully updated';
+    //             this.loadStudents();
+    //         });
+    //         this.editedStudent = null;
+    //     }
+    // }
+    //
+    // cancel(): void {
+    //     if (this.isNewRecord) {
+    //         this.students.pop();
+    //         this.isNewRecord = false;
+    //     }
+    //     this.editedStudent = null;
+    // }
+    //
+    // deleteStudent(student: Student): void {
+    //     this.studentService.deleteStudent(student.id).subscribe(data => {
+    //         this.statusMessage = 'Student was successfully deleted';
+    //         this.loadStudents();
+    //     });
+    // }
 }
--- frontend/src/app/modules/app.module.ts
+++ frontend/src/app/modules/app.module.ts
@@ -4,7 +4,6 @@ import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from "../components/app.component";
 import {MatTableModule} from "@angular/material/table";
-import {MatTableDataSource} from "@angular/material/table";
 
 @NgModule({
     imports: [BrowserModule, FormsModule, HttpClientModule, MatTableModule],
